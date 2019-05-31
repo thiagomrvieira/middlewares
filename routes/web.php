fd754295bea3c9279733ef79f95ba58d51df67e2
Thiago Maciel <thiagomrvieira@gmail.com>
2019-05-31 15:11:10 -0300

    Introdução

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Middleware\PrimeiroMiddleware;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/usuarios', 'UsuarioControlador@index')->middleware(PrimeiroMiddleware::class);
